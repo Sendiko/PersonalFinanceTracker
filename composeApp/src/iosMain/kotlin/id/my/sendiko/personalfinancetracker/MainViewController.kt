722edfc180bef96e2cf95e5e5414154dd533d742
Sendiko <sarangtawon897@gmail.com>
2025-03-04 14:22:20 +0700

    implement the new theming

--- composeApp/src/iosMain/kotlin/id/my/sendiko/personalfinancetracker/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/id/my/sendiko/personalfinancetracker/MainViewController.kt
@@ -1,5 +1,14 @@
 package id.my.sendiko.personalfinancetracker
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIScreen
+import platform.UIKit.UIUserInterfaceStyle
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val isDarkTheme = UIScreen.mainScreen.traitCollection.userInterfaceStyle ==
+            UIUserInterfaceStyle.UIUserInterfaceStyleDark
+    App(
+        darkTheme = isDarkTheme,
+        dynamicColor = false
+    )
+}
